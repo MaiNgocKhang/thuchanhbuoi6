f8b0dbbc35499a8ac7c66c1a1bdae88723ba81d3
Mai Ngoc Khang <maingckhang8925@gmail.com>
2023-10-04 16:08:48 +0700

    buoi 7

--- WindowsFormsApplication1/Form3.Designer.cs
+++ WindowsFormsApplication1/Form3.Designer.cs
@@ -28,11 +28,210 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtPhongban = new System.Windows.Forms.TextBox();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnThempB = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMa = new System.Windows.Forms.TextBox();
+            this.txtten = new System.Windows.Forms.TextBox();
+            this.txtdiachi = new System.Windows.Forms.TextBox();
+            this.cbxPhongban = new System.Windows.Forms.ComboBox();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtPhongban);
+            this.groupBox1.Controls.Add(this.btnXoa);
+            this.groupBox1.Controls.Add(this.btnThempB);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.treeView1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 270);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Phòng ban";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // txtPhongban
+            // 
+            this.txtPhongban.Location = new System.Drawing.Point(74, 160);
+            this.txtPhongban.Name = "txtPhongban";
+            this.txtPhongban.Size = new System.Drawing.Size(120, 20);
+            this.txtPhongban.TabIndex = 4;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(74, 233);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(120, 28);
+            this.btnXoa.TabIndex = 3;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnThempB
+            // 
+            this.btnThempB.Location = new System.Drawing.Point(74, 199);
+            this.btnThempB.Name = "btnThempB";
+            this.btnThempB.Size = new System.Drawing.Size(120, 28);
+            this.btnThempB.TabIndex = 2;
+            this.btnThempB.Text = "Thêm";
+            this.btnThempB.UseVisualStyleBackColor = true;
+            this.btnThempB.Click += new System.EventHandler(this.btnThempB_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 163);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "phòng ban";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(7, 20);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(187, 133);
+            this.treeView1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(228, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mã Số";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(379, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Họ Tên";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(225, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Địa Chỉ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(225, 128);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Phòng ban";
+            // 
+            // txtMa
+            // 
+            this.txtMa.Location = new System.Drawing.Point(273, 22);
+            this.txtMa.Name = "txtMa";
+            this.txtMa.Size = new System.Drawing.Size(100, 20);
+            this.txtMa.TabIndex = 5;
+            // 
+            // txtten
+            // 
+            this.txtten.Location = new System.Drawing.Point(418, 19);
+            this.txtten.Name = "txtten";
+            this.txtten.Size = new System.Drawing.Size(100, 20);
+            this.txtten.TabIndex = 6;
+            // 
+            // txtdiachi
+            // 
+            this.txtdiachi.Location = new System.Drawing.Point(286, 81);
+            this.txtdiachi.Name = "txtdiachi";
+            this.txtdiachi.Size = new System.Drawing.Size(232, 20);
+            this.txtdiachi.TabIndex = 7;
+            // 
+            // cbxPhongban
+            // 
+            this.cbxPhongban.FormattingEnabled = true;
+            this.cbxPhongban.Location = new System.Drawing.Point(291, 119);
+            this.cbxPhongban.Name = "cbxPhongban";
+            this.cbxPhongban.Size = new System.Drawing.Size(227, 21);
+            this.cbxPhongban.TabIndex = 8;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(228, 160);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(120, 28);
+            this.btnThem.TabIndex = 9;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(382, 160);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(120, 28);
+            this.btnThoat.TabIndex = 10;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 294);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.cbxPhongban);
+            this.Controls.Add(this.txtdiachi);
+            this.Controls.Add(this.txtten);
+            this.Controls.Add(this.txtMa);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form3";
             this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox txtPhongban;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnThempB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMa;
+        private System.Windows.Forms.TextBox txtten;
+        private System.Windows.Forms.TextBox txtdiachi;
+        private System.Windows.Forms.ComboBox cbxPhongban;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnThoat;
     }
 }
--- WindowsFormsApplication1/Form3.cs
+++ WindowsFormsApplication1/Form3.cs
@@ -16,5 +16,68 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+           
+        }
+        public bool kiemtra(string s)
+        {
+            if (string.Compare(s, treeView1.SelectedNode.Text, true) == 0)
+                return true;
+            return false;
+        }
+
+        private void btnThempB_Click(object sender, EventArgs e)
+        {
+            
+            if (kiemtra(txtPhongban.Text)== false)
+            {
+                treeView1.Nodes.Add(txtPhongban.Text);
+                cbxPhongban.Items.Add(txtPhongban.Text);
+            }
+            else
+                MessageBox.Show("Phòng ban đã tồn tại!");
+            txtPhongban.Text = "";
+            txtPhongban.Focus();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            string[] pb = { "Giám Đốc", "Tổ chức hành chính", "Kế hoạch", "Kế Toán" };
+            foreach (string s in pb)
+            {
+                treeView1.Nodes.Add(s);
+                cbxPhongban.Items.Add(s);
+            }
+            cbxPhongban.SelectedIndex = 0;
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Bạn có chắc muốn xóa?","thông báo",MessageBoxButtons.YesNo,MessageBoxIcon.Question,MessageBoxDefaultButton.Button1)==DialogResult.Yes)
+            {
+                if(treeView1.SelectedNode != null)
+                {
+                    cbxPhongban.Items.Remove(treeView1.SelectedNode.Text);
+                    treeView1.Nodes.Remove(treeView1.SelectedNode);
+                }
+            }
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            int index = -1;
+            foreach(TreeNode node in treeView1.Nodes)
+                if(node.Text == cbxPhongban.Text)
+                {
+                    index = node.Index;
+                    break;
+                }
+            treeView1.Nodes[index].Nodes.Add(txtten.Text + "(" + txtMa.Text + ")");
+            treeView1.ExpandAll();
+        }
+
+        
     }
 }
